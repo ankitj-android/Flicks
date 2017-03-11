53d640757e873e1b3138be8b750a4e1b17543cbf
Ankit Jasuja <AJasuja@walmartlabs.com>
2017-03-10 20:45:49 -0800

    view holder pattern added for caching views.

--- app/src/main/java/com/ajasuja/codepath/flicks/adapter/MoviesAdapter.java
+++ app/src/main/java/com/ajasuja/codepath/flicks/adapter/MoviesAdapter.java
@@ -15,11 +15,20 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import static com.ajasuja.codepath.flicks.R.id.textViewMovieOverview;
+import static com.ajasuja.codepath.flicks.R.id.textViewMovieTitle;
+
 /**
  * Created by ajasuja on 3/10/17.
  */
 
 public class MoviesAdapter extends ArrayAdapter<Movie> {
+
+    private static class ViewHolder {
+        private ImageView imageViewPosterImage;
+        private TextView textViewMovieTitle;
+        private TextView textViewMovieOverview;
+    }
 
     public MoviesAdapter(Context context, List<Movie> movies) {
         super(context, android.R.layout.simple_list_item_1, movies);
@@ -28,24 +37,26 @@ public class MoviesAdapter extends ArrayAdapter<Movie> {
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        //
         Movie movie = getItem(position);
+        ViewHolder viewHolder;
 
         if (convertView == null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.item_movie, parent, false);
+
+            viewHolder = new ViewHolder();
+            viewHolder.imageViewPosterImage = (ImageView) convertView.findViewById(R.id.imageViewPosterImage);
+            viewHolder.textViewMovieTitle = (TextView) convertView.findViewById(textViewMovieTitle);
+            viewHolder.textViewMovieOverview = (TextView) convertView.findViewById(textViewMovieOverview);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        ImageView imageViewPosterImage = (ImageView) convertView.findViewById(R.id.imageViewPosterImage);
-        imageViewPosterImage.setImageResource(0); // clear out image from convertview
-
-        
-        Picasso.with(getContext()).load(movie.getPosterPath()).into(imageViewPosterImage);
-        TextView textViewMovieTitle = (TextView) convertView.findViewById(R.id.textViewMovieTitle);
-        TextView textViewMovieOverview = (TextView) convertView.findViewById(R.id.textViewMovieOverview);
-
-        textViewMovieTitle.setText(movie.getTitle());
-        textViewMovieOverview.setText(movie.getOverview());
+        viewHolder.imageViewPosterImage.setImageResource(0); // clear out image from convertview
+        Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.imageViewPosterImage);
+        viewHolder.textViewMovieTitle.setText(movie.getTitle());
+        viewHolder.textViewMovieOverview.setText(movie.getOverview());
 
         return convertView;
     }
